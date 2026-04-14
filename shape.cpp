0a7fc0604dd25677c666ba85b730045d981e4431
kfaust-art <kfaust@ualr.edu>
2026-04-14 11:41:47 -0500

    Refactor Shape class by removing display methods
    Removed specific shape display methods for Rectangle and Circle.

--- shape.cpp
+++ shape.cpp
@@ -1,14 +1,8 @@
 #include "Shape.h"
 #include <iostream>
 
+#include "Shape.h"
+
 void Shape::display() const {
     std::cout << "Generic Shape";
-}
-
-void Rectangle::display() const {
-    std::cout << "Rectangle (" << width << " x " << height << ")";
-}
-
-void Circle::display() const {
-    std::cout << "Circle (radius: " << radius << ")";
 }
