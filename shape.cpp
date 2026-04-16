72f997d36a7b35bcaff2b5ec7b83a1f4beae0310
kfaust-art <kfaust@ualr.edu>
2026-04-15 22:29:33 -0500

    Update shape.cpp

--- shape.cpp
+++ shape.cpp
@@ -1,13 +1,2 @@
 #include "Shape.h"
-
-void Shape::display() const {
-    std::cout << "Basic Shape type";
-}
-
-void Rectangle::display() const {
-    std::cout << "Rectangle (" << width << " x " << height << ")";
-}
-
-void Circle::display() const {
-    std::cout << "Circle (radius: " << radius << ")";
-}
+// If you had more complex logic for display(), it would go here.
